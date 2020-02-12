4cd364ebb9c54c9070f4d13ce2d643429b65d313
KitmanNorth <thanasak.phomiket@gmail.com>
2020-02-12 16:11:59 +0700

    add simulating buy/sell stock(when balance>close) and print out buy-sell/net balance

--- server/api/getSimulatedStock.js
+++ server/api/getSimulatedStock.js
@@ -1,19 +1,76 @@
 const pool = require('./pool-connection')
 
 const getSimulatedStock = (req, res) => {
-    symbol = 'GULF'
-    beginDate = '2019-08-01'
-    endDate = '2019-09-30'
-    // what about amount ?? talk with man
+
+    symbol = [{"name":"GULF", "amount": 10000}, {"name":"GPSC", "amount": 5000}]
+    date = [{"beginDate": '2019-08-01', "endDate": '2019-10-31'}]
+
+    // all symbol string query
+    allStockStr = "("
+    for(i=0; i<symbol.length; i++){
+        allStockStr += "symbol = '" + symbol[i].name + "' "
+        if(i != symbol.length-1){
+            allStockStr += "or "
+        }else{
+            allStockStr += ") "
+        }
+    }
+
     queryStr = "select date,symbol,open,high,low,close,percentChange,volume,money,status from trade \
-    where symbol = '" + symbol + "' \
+    where " + allStockStr + " \
     and (status = '1' or status = '-1') \
-    and date between '" + beginDate + "' and '" + endDate + "';"
+    and date between '" + date[0].beginDate + "' and '" + date[0].endDate + "';"
     pool.query(queryStr , (err, results) => {
         if(err){
             throw err
         }
+
         res.status(200).json(results.rows)
+
+        // one stock
+        balance = symbol[0].amount
+        numOfStock = 0
+        for(i = 0; i<results.rows.length; i++){
+            if(results.rows[i].symbol == symbol[0].name){
+                if(balance > results.rows[i].close){
+                    //buy
+                    if(results.rows[i].status == '1'){
+                        buyAmount = balance/2.0
+                        stockPrice = results.rows[i].close
+                        numOfBuyStock = parseInt(buyAmount/stockPrice)
+                        buyAmount = stockPrice*numOfBuyStock
+                        numOfStock += numOfBuyStock
+                        balance -= buyAmount
+                        console.log("Buy")
+                        console.log(balance)
+                    } 
+                    else {
+                        stockPrice = results.rows[i].close
+                        sellAmount = stockPrice*numOfStock
+                        balance += sellAmount
+                        numOfStock = 0
+                        console.log("Sell")
+                        console.log(balance)
+                    }
+                }
+            }
+        }
+        // Check net balance
+        stockPrice = results.rows[results.rows.length-1].close
+        sellAmount = stockPrice*numOfStock
+        balance += sellAmount
+        numOfStock = 0
+        console.log("Total Balance After sell all stock:")
+        console.log(balance)
+            
+
+        // historyList = []
+        // for(i = 0; i<results.rows.length; i++){
+        //     if(results.rows[i].status = '1'){
+        //         historyList.push(results.rows[i])
+        //     }
+        // }
+        // res.status(200).json(historyList)
     })
 }
 
