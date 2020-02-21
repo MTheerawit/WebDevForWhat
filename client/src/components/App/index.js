acc3fae99b0de3de6d2a6831791c6644f3329224
Theerawit <t_heerawit@hotmail.com>
2020-02-22 02:31:06 +0700

    wowowow

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,45 +2,85 @@ import React from 'react';
 import './style.css';
 import { Badge } from 'react-bootstrap';
 import '../../../node_modules/antd/dist/antd.css';
-import { DatePicker,InputNumber,Table } from 'antd';
+import { Table } from 'antd';
 
-const {RangePicker} = DatePicker;
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
-    width: 150,
+    title: 'Date',
+    dataIndex: 'date',
+    width: 120,
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    width: 150,
+    title: 'Symbol',
+    dataIndex: 'symbol',
+    width: 120,
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
+    title: 'Open',
+    dataIndex: 'open',
+    width: 70,
+  },
+  {
+    title: 'High',
+    dataIndex: 'high',
+    width: 70,
+  },
+  {
+    title: 'Low',
+    dataIndex: 'low',
+    width: 70,
+  },
+  {
+    title: 'Last',
+    dataIndex: 'close',
+    width: 70,
+  },
+  {
+    title: 'Change%',
+    dataIndex: 'percentchange',
+    width: 95,
+  },
+  {
+    title: 'TotalVolume(shares)',
+    dataIndex: 'volume',
+    width: 165,
+  },
+  {
+    title: 'TotalVolume("000 bath)',
+    dataIndex: 'money',
+    width: 185,
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    width: 70,
   },
 ];
 const data = [];
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 20; i++) {
   data.push({
     key: i,
-    name: `Edward King ${i}`,
-    age: 32,
-    address: `London, Park Lane no. ${i}`,
+    date: '2019-08-15',
+    symbol: 'ADVANC',
+    open: '219',
+    high: '219',
+    low: '215',
+    close: '216',
+    percentchange: '-3.57',
+    volume: '145057103',
+    money: '777777777.777',
+    status: '0',
   });
-}
-function onChangeDate(date, dateString) {
-  console.log(date, dateString);
-}
-function onChangeNumber(value) {
-  console.log('changed', value);
 }
 
 class App extends React.Component{
 
-  boxsearch = (keyword)=>{
-    console.log(keyword)
+  BoxInputSearch = (keyword)=>{
+    console.log('search : ',keyword);
+  }
+
+  BoxInputNumber = (number)=>{
+    console.log('inputnum : ', number);
   }
 
   render(){
@@ -55,16 +95,9 @@ class App extends React.Component{
             </tr>
           </tbody>
         </table>
-        <br />
-        <input className="boxsearch" placeholder="Search..."  onChange={(event)=>{this.searchbox(event.target.value)}}/>
-        <br />
-        <RangePicker onChange={onChangeDate} />
-        <br />
-        {/* <InputNumber size="large" min={0} max={100000} defaultValue={0} onChange={onChangeNumber} /> */}
-        <InputNumber min={0} max={100000} defaultValue={0} onChange={onChangeNumber} />
-        <br />
-        <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />,
-
+        <input className="boxinput" placeholder="Search..."  onChange={(event)=>{this.BoxInputSearch(event.target.value)}}/>
+        <input className="boxinput" placeholder="0"  onChange={(event)=>{this.BoxInputNumber(event.target.value)}}/>
+        <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 600 }} />,
       </div>
     );
   }
