23675bb02bc888b846ccbcf951043aadfc7b2e61
MTheerawit <T_heerawit@hotmail.com>
2020-03-14 12:05:00 +0700

    .

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import Header from '../../../src/components/Header/Header'
-import TableS from '../Table/TableS'
 
 export default class App extends React.Component{
   render(){
