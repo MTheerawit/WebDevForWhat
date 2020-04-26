a21471516ca058ec0841958e489f6ba0b1784a6c
Theerawit <t_heerawit@hotmail.com>
2020-04-26 20:41:43 +0700

    mayuuuuu

--- client/src/components/Table/TableR.js
+++ client/src/components/Table/TableR.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../../../node_modules/antd/dist/antd.css';
 import { Table, Tag } from 'antd';
 
+import {  Grid, Dropdown,Button } from 'semantic-ui-react'
+import _ from "lodash";
+
 import RecommendToday from '../Badge/RecommendToday';
 import Title from 'antd/lib/skeleton/Title';
 
@@ -76,7 +79,18 @@ const columns = [
 
 export default class TableR extends React.Component{
   state = {
-    data: []
+    data: [],
+    Year: '',
+    Month: '',
+    Day: '',
+  }
+  ClickNext = ()=>{
+    console.log('muyuuu')
+  }
+  handleChangeDate = (e, { name, value }) => {
+    this.setState({ [name]: value })
+    console.log(`selected ${value}`)
+    console.log({[name]: value})
   }
   callAPI() {
     fetch("http://localhost:9000/getRecStock")
@@ -90,13 +104,60 @@ export default class TableR extends React.Component{
     this.callAPI()
   }
   render(){
+    const getOptions = (number, prefix = "") =>
+      _.times(number, index => ({
+          key: index,
+          text: `${prefix}${index + 1}`,
+          value: index+1
+      }));
+    const getOptionsYear = (number, prefix = "") =>
+      _.times(number, index => ({
+          key: index,
+          text: `${prefix}${index + 2019}`,
+          value: index+2019
+      }));
+    const { value } = this.state
     return(
       <div>
+
         <Title/>
         <RecommendToday/>
         <Title/>
+
+        <Grid columns='equal'>
+          <Grid.Column>
+            <Dropdown
+              selection
+              name = 'Year'
+              options={getOptionsYear(2)}
+              onChange={this.handleChangeDate }
+              placeholder='Year'
+              value={value}
+            />
+            <Dropdown
+              selection
+              name = 'Month'
+              options={getOptions(12)}
+              onChange={this.handleChangeDate }
+              placeholder='Month'
+              value={value}
+            />
+            <Dropdown
+              selection
+              name = 'Day'
+              options={getOptions(31)}
+              onChange={this.handleChangeDate }
+              placeholder='Day'
+              value={value}
+            />
+            <Button color="teal" onClick={this.ClickNext} >mayu</Button>
+          </Grid.Column>
+        </Grid>
+
+        <Title/>
         <Table columns={columns} dataSource={this.state.data} pagination={{ pageSize: 10 }} scroll={{ x: 1000,y: 600 }} />
         <Title/>
+
       </div>
     )
   }
